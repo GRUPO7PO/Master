54033c88300a4defc7c5dde7b78f38f1907577b6
Marlon Galo <dukegalo@gmail.com>
2022-07-13 22:49:59 -0600

    Merge origin/master

--- src/main/java/helpers/Persona.java
+++ src/main/java/helpers/Persona.java
@@ -9,4 +9,46 @@ package helpers;
  * @author Marlon Galo
  */
 public abstract class Persona {
+    //Atributos
+    private String nombre;
+    private String apellido;
+    private String dni;
+
+
+    //Constructor sin parametros
+    public Persona() {
+    }
+    
+    //Establecer y Obtener
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public String obtenerApellido() {
+        return apellido;
+    }
+
+    public String obtenerDni() {
+        return dni;
+    }
+
+    public void establecerNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void establecerApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void establecerDni(String dni) {
+        this.dni = dni;
+    }
+
+    
+    //Mostrar o imprimir informacion
+    public String mostrarDatos(){
+        System.out.println("Nombre: " +nombre +"\nApellido: " +apellido +"\nDocumento Nacional de Identificacion: " +dni);
+        return null;
+    }
+    
 }
